d52e7e0fe3278970b6c1ce8f36b8d967c5897dd9
Maikel Karam <maikel.m.karam@gmail.com>
2023-01-17 10:33:02 -0600

    homecontroller and mathcontroller completed

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HomeController {
+
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "This is the landing page!";
+    }
+
+}
