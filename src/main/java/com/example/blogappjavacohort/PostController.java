a59df3ef99c2eaa96a9e884967bc4ca60f878fce
Maikel Karam <maikel.m.karam@gmail.com>
2023-01-17 11:09:06 -0600

    post controller exercise completed

--- src/main/java/com/example/blogappjavacohort/PostController.java
+++ src/main/java/com/example/blogappjavacohort/PostController.java
@@ -1,6 +1,33 @@
 package com.example.blogappjavacohort;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class PostController {
 
+    @GetMapping(path = "/posts")
+    @ResponseBody
+    public String hello() {
+        return "This is the index page!";
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String viewPostbyID(@PathVariable int id) {
+        return "This will view an individual post";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String createForm () {
+        return "This is the form to  create posts";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String postForm () {
+        return "Upload created posts";
+    }
 
 }
