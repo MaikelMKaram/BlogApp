8ddd135d8f93afee7e45309bc82b4d5fb554251c
Maikel Karam <maikel.m.karam@gmail.com>
2023-01-17 10:41:27 -0600

    progress save

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -1,10 +1,11 @@
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class HomeController {
 
-
-    @GetMapping("/")
+    @GetMapping(path = "/")
     @ResponseBody
     public String hello() {
         return "This is the landing page!";
--- src/main/java/MathController.java
+++ src/main/java/MathController.java
@@ -1,8 +1,10 @@
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class MathController {
     @RequestMapping(path = "/add/{number}/and/{number2}", method = RequestMethod.GET)
     @ResponseBody
