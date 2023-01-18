6fbaa6cbf0cb715940fefa9e0db2b2c417f93666
Maikel Karam <maikel.m.karam@gmail.com>
2023-01-18 09:22:19 -0600

    view exercise 1 completed

--- src/main/java/com/example/blogappjavacohort/PostController.java
+++ src/main/java/com/example/blogappjavacohort/PostController.java
@@ -1,7 +1,10 @@
 package com.example.blogappjavacohort;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
@@ -28,6 +31,38 @@ public class PostController {
     @ResponseBody
     public String postForm () {
         return "Upload created posts";
+    }
+
+    //View exercise 1
+    @RequestMapping(path="/roll/dice", method = RequestMethod.GET)
+    public  String roll(Model model){
+        ArrayList<Integer> ids = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            int id = i + 1;
+            ids.add(id);
+        }
+        model.addAttribute("id", ids);
+        return "button";
+    }
+
+    @PostMapping(path="/roll/dice")
+    String id(@RequestParam(name = "id") String idd, Model model ) throws Exception{
+        System.out.println(idd);
+        model.addAttribute("id", idd);
+        System.out.println(idd);
+        return "redirect:/roll/" + idd;
+    }
+
+    @RequestMapping(path = "/roll/{n}", method = RequestMethod.GET)
+    String ret(@PathVariable String n, Model model ) {
+        int rando = (int) Math.floor(Math.random() * 6 + 1);
+        boolean check = String.valueOf(rando).equals(n);
+        if (check) {
+            model.addAttribute("check", check);
+        } else model.addAttribute("check", check);
+        model.addAttribute("id", String.valueOf(rando));
+        model.addAttribute("n", n);
+        return "check";
     }
 
 }
