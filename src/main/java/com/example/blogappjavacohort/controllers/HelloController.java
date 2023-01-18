01845e78caea123857f60819fbfce27828f0d367
Maikel Karam <maikel.m.karam@gmail.com>
2023-01-18 09:35:20 -0600

    progress save

--- src/main/java/com/example/blogappjavacohort/controllers/HelloController.java
+++ src/main/java/com/example/blogappjavacohort/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.blogappjavacohort;
+package com.example.blogappjavacohort.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
