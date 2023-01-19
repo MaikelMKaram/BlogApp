31c9541bb352a38db939569125af300114044d4a
Maikel Karam <maikel.m.karam@gmail.com>
2023-01-19 13:28:41 -0600

    progress save

---
+++ src/main/java/com/example/blogappjavacohort/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.codeup.repository;
+
+import com.codeup.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User getById(long id);
+    User findByUsername(String name);
+}
